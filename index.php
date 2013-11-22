01c77bd29e1305bfb7e42a18735073c33a53d076
FalconGames <falcongames109@gmail.com>
2013-11-22 14:57:18 -0500

    Added basic homepage

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <!DOCTYPE html>
+<html>
+  <body style="background-color:#fff">
+    <div align="CENTER" style="background-color:#ccc;font-family:verdana">
+      <h1>Login</h1>
+      <form action="getuser.php" method="get">
+        Username: <input type="text" name="u"><input type="submit" value="Login!">
+      </form>
+    </div>
+  </body>
+</html>
