1b9603873c90c124d0edff30a94fcb3fafd0e9e4
ENM185 <karatekid185@gmail.com>
2013-11-25 14:35:31 -0500

    More colors

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <br><br><br><br><br>
     <div align="CENTER" style="background-color:#333;font-family:arial;color:#202020">
       <h1 style="color:#202020">Login</h1>
-      <form action="getuser.php" method="post" style="color:#202020">
-        Username: <input type="text" name="u">
-        Password: <input type="password" name="p"><input type="submit" value="Login!">
+      <form action="getuser.php" method="post" style="color:#202020;">
+        Username: <input type="text" name="u" style="background-color:#202020;color:#333">
+        Password: <input type="password" name="p" style="background-color:#202020;color:#333"><input type="submit" value="Login!">
       </form>
       <br><br><br><br><br><br><br>
     </div>
