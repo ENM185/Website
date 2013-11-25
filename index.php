89623aaf52f4c806f2e5e2a4a8704b6a61d18ea4
ENM185 <karatekid185@gmail.com>
2013-11-25 14:30:52 -0500

    text color

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
   <body style="background-color:#202020">
     <br><br><br><br><br>
     <div align="CENTER" style="background-color:#333;font-family:arial;color:#202020">
-      <h1>Login</h1>
-      <form action="getuser.php" method="post">
+      <h1 style="color:#202020">Login</h1>
+      <form action="getuser.php" method="post" style="color:#202020">
         Username: <input type="text" name="u">
         Password: <input type="password" name="p"><input type="submit" value="Login!">
       </form>
