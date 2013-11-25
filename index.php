6b7da7e9ba7c39db73a42ab015c55f3ae9385721
ENM185 <karatekid185@gmail.com>
2013-11-25 11:10:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <body style="background-color:#fff">
     <div align="CENTER" style="background-color:#ccc;font-family:verdana">
       <h1>Login</h1>
-      <form action="getuser.php" method="get">
+      <form action="getuser.php" method="post">
         Username: <input type="text" name="u">
         Password: <input type="text" name="p"><input type="submit" value="Login!">
       </form>
