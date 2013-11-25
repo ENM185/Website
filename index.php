1685d8af35057f1c910fb68ac9e87687ad8ee625
FalconGames <falcongames109@gmail.com>
2013-11-25 12:24:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
   <body style="background-color:#fff">
+    <br><br><br><br><br>
     <div align="CENTER" style="background-color:#ccc;font-family:verdana">
       <h1>Login</h1>
       <form action="getuser.php" method="post">
         Username: <input type="text" name="u">
         Password: <input type="text" name="p"><input type="submit" value="Login!">
       </form>
+      <br><br><br><br><br><br><br>
     </div>
   </body>
 </html>
