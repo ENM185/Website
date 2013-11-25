acc03d066b10a6db69b45083b7b4a30244e97a0c
ENM185 <karatekid185@gmail.com>
2013-11-25 14:29:26 -0500

    div text color

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
   <body style="background-color:#202020">
     <br><br><br><br><br>
-    <div align="CENTER" style="background-color:#333;font-family:arial">
+    <div align="CENTER" style="background-color:#333;font-family:arial;color:#202020">
       <h1>Login</h1>
       <form action="getuser.php" method="post">
         Username: <input type="text" name="u">
