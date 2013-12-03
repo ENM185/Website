054c19a7a0c65495006e0e7e691e47f80faf1b6f
ENM185 <karatekid185@gmail.com>
2013-12-03 10:59:36 -0500

    made text in text fields more visible

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <div align="CENTER" style="background-color:#333;font-family:arial;color:#202020">
       <h1 style="color:#202020">Login</h1>
       <form action="getuser.php" method="post" style="color:#202020;">
-        Username: <input type="text" name="u" style="background-color:#202020;color:#333">
-        Password: <input type="password" name="p" style="background-color:#202020;color:#333"><input type="submit" value="Login!">
+        Username: <input type="text" name="u" style="background-color:#202020;color:#FFFFFF">
+        Password: <input type="password" name="p" style="background-color:#202020;color:#FFFFFF"><input type="submit" value="Login!">
       </form>
       <br><br><br><br><br><br><br>
     </div>
